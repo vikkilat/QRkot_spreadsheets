d778adaeefefdaf92d9adbdf45bd19ef4136d09c
Виктория Латышева <vikkilat1989@yandex.ru>
2023-09-26 21:44:54 +0300

    After review. 26.09.2023

--- app/services/google_api.py
+++ app/services/google_api.py
@@ -16,6 +16,15 @@ SPREADSHEET_BODY = {
                                                   'columnCount': 11}}}]
 }
 
+PERMISSIONS_BODY = {'type': 'user',
+                    'role': 'writer',
+                    'emailAddress': settings.email}
+
+TABLE_VALUES = [
+    ['Отчет от', datetime.now().strftime(FORMAT)],
+    ['Топ проектов по скорости закрытия'],
+    ['Название проекта', 'Время сбора', 'Описание']]
+
 
 async def spreadsheets_create(wrapper_services: Aiogoogle) -> str:
     SPREADSHEET_BODY['properties']['title'] = 'Отчет на {}'.format(datetime.now().strftime(FORMAT))
@@ -32,14 +41,11 @@ async def set_user_permissions(
     spreadsheet_id: str,
     wrapper_services: Aiogoogle
 ) -> None:
-    permissions_body = {'type': 'user',
-                        'role': 'writer',
-                        'emailAddress': settings.email}
     service = await wrapper_services.discover('drive', 'v3')
     await wrapper_services.as_service_account(
         service.permissions.create(
             fileId=spreadsheet_id,
-            json=permissions_body,
+            json=PERMISSIONS_BODY,
             fields="id"
         ))
 
@@ -49,22 +55,15 @@ async def spreadsheets_update_value(
     projects: list,
     wrapper_services: Aiogoogle
 ) -> None:
-    now_date_time = datetime.now().strftime(FORMAT)
     service = await wrapper_services.discover('sheets', 'v4')
-
-    table_values = [
-        ['Отчет от', now_date_time],
-        ['Топ проектов по скорости закрытия'],
-        ['Название проекта', 'Время сбора', 'Описание']
-    ]
 
     for proj in projects:
         new_row = [str(proj['name']), str(proj['project_lifetime']), str(proj['description'])]
-        table_values.append(new_row)
+        TABLE_VALUES.append(new_row)
 
     update_body = {
         'majorDimension': 'ROWS',
-        'values': table_values
+        'values': TABLE_VALUES
     }
     await wrapper_services.as_service_account(
         service.spreadsheets.values.update(
